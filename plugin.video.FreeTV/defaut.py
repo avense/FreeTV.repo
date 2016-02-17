27b9b2ad3b85b866295c0bd2f05763d968d75977
h3ld3rkid <helderkid@gmail.com>
2016-02-17 21:58:11 +0000

    Versao 3

--- plugin.video.FreeTV/defaut.py
+++ plugin.video.FreeTV/defaut.py
@@ -13,13 +13,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 ##############BIBLIOTECAS A IMPORTAR E DEFINICOES####################
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,HTMLParser,base64,xmltosrt,os
+import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,HTMLParser,os,base64
+from BeautifulSoup import BeautifulStoneSoup, BeautifulSoup, BeautifulSOAP
 from BeautifulSoup import BeautifulSoup
 h = HTMLParser.HTMLParser()
-
 
 versao = '0.0.1'
 addon_id = 'plugin.video.FreeTV'
@@ -27,48 +26,26 @@ selfAddon = xbmcaddon.Addon(id=addon_id)
 addonfolder = selfAddon.getAddonInfo('path')
 artfolder = addonfolder + '/resources/img/'
 fanart = addonfolder + '/fanart.png'
+url_base = base64.b64decode('aHR0cDovL3R2LW1zbi5jb20vbWVzdHJlLnBocA==')
+url_base2 = base64.b64decode('aHR0cDovL3R2LW1zbi5jb20vY2FuYWlzLmh0bWw=')
+url_base3 = base64.b64decode('aHR0cDovL3d3dy50di1tc24uY29tL3BsYXllci9wbGF5ZXIuc3dm')
+url_base4 = base64.b64decode('aHR0cHM6Ly9jb3B5LmNvbS9oWjJFbU1YZnROdTRSV3oxP2Rvd25sb2FkPTE=')
+url_base5 = base64.b64decode('aHR0cDovL3d3dy5hb3Zpdm9icmFzaWwuY29tL3R2YW1pZ29zMi8=')
+url_base6 = base64.b64decode('aHR0cDovL3d3dy5jYXJvbGluZW9saXZlaXJhLmNvbS5ici9zd2YvcGxheWVyLnN3Zg==')
  
-
 ###################################################MENUS############################################
-def menu():
-    addDir('Canais Tv','-',3,'')
-    
-def categorias():
-    addDir('Eventos Principais AceStream/Plexus','http://pastebin.com/raw.php?i=JSxSwLgT',6,'http://anonymousoft.com/wp-content/uploads/2014/09/Ace-Stream-Media-Full-Free-Download.jpg')
-    addDir('Canais Gerais','http://pastebin.com/raw.php?i=cHKGyc3b',6,'https://appsportugal.com/media/uploads/logo-2.jpg')
-    addDir('Desporto','http://pastebin.com/6KqM4GPK',6,'http://www.sefo.pt/ficheiros/varias-imagens/esporto_1_.gif')
-    
-###############################################################FKav####################################################
-def gethtml(url):
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-    response = urllib2.urlopen(req)
-    link = response.read()
-    soup = BeautifulSoup(link)
-    return soup
 
-def player_youtube(url):
-    #mera correção feita por Cleiton Leonel Creton!!!
-	xbmcPlayer = xbmc.Player()
-        xbmcPlayer.play('plugin://plugin.video.youtube/play/?video_id=' +url)
-		
-def listar_videostxt(url):
-      for line in urllib2.urlopen(url).readlines():
-            params = line.split(',')
-            try:
-                  nome = params[0]
-                  print 'Nome: ' + nome
-                  img = params[1].replace(' http','http')
-                  print 'Img: ' + img
-                  rtmp = params[2]
-                  print 'Link: ' + rtmp
-                  addDir(nome,rtmp,11,img,False)
-            except:
-                  pass
-      xbmc.executebuiltin("Container.SetViewMode(500)")
-		
-##########################################################################################################################	
+def  menus():        		
+	dialog = xbmcgui.Dialog()
+	addDir('[B]Canais TV Free[/B]','-',2,'')	
 	
+def  categorias():
+	addDir('[B]Portugueses[/B]','http://pastebin.com/raw/cHKGyc3b',4,'https://copy.com/JvvK4Iw39ofK1rPF')
+	addDir('[B]Desporto[/B]','https://xxx.com',4,'https://copy.com/NPRaZORymdG0FxJ1')
+		
+
+###############################################################FKav####################################################
+
 def listar_canais(url):
       for line in urllib2.urlopen(url).readlines():
             params = line.split(',')
@@ -82,9 +59,114 @@ def listar_canais(url):
                   addLink(nome,rtmp,img)
             except:
                   pass
-      xbmc.executebuiltin("Container.SetViewMode(500)")
-	  
-	  
+      xbmc.executebuiltin("Container.SetViewMode(500)")		
+		
+def listar_videostxt(url):
+      for line in urllib2.urlopen(url).readlines():
+            params = line.split(',')
+            try:
+                  nome = params[0]
+                  print 'Nome: ' + nome
+                  img = params[1].replace(' http','http')
+                  print 'Img: ' + img
+                  rtmp = params[2]
+                  print 'Link: ' + rtmp
+                  addDir(nome,rtmp,6,img,False)
+            except:
+                pass
+		xbmc.executebuiltin("Container.SetViewMode(500)")
+
+def player_youtube(url):
+    #mera correção feita por Cleiton Leonel Creton!!!
+	xbmcPlayer = xbmc.Player()
+	xbmcPlayer.play('plugin://plugin.video.youtube/play/?video_id=' +url)
+
+def listar_categorias():
+	html = gethtml(url_base4)
+	soup = html.find("div",{"class":"canais"})
+	canais = soup.findAll("li")
+	for canal in canais:
+		titulo = canal.a.text
+		url = canal.a["href"]
+		iconimage = canal.img["src"]
+		addDir("[B]"+titulo.encode('utf-8')+"[/B]",url,8,iconimage)
+        xbmcplugin.setContent(int(sys.argv[1]), 'episodies')
+	xbmc.executebuiltin('Container.SetViewMode(500)')	
+	
+def canais_master(name,url,iconimage):
+	html = gethtml(url)
+	soup = html.find("div",{"class":"canais"})
+	canais = soup.findAll("li")
+	for canal in canais:
+		titulo = canal.a.text
+		url = canal.a["href"]
+		iconimage = canal.img["src"]
+		addDir("[B]"+titulo.encode('utf-8')+"[/B]",url,10,iconimage,False)
+        xbmcplugin.setContent(int(sys.argv[1]), 'episodies')
+	xbmc.executebuiltin('Container.SetViewMode(500)')
+		
+def series_e_desenhos_24hrs():
+	html = gethtml(base64.b64decode('aHR0cHM6Ly9jb3B5LmNvbS82Y1ZYMmJQR0xzOFBjQ0xqP2Rvd25sb2FkPTE='))
+	soup = html.find("div",{"class":"canais"})
+	items = soup.findAll("li")
+	for item in items:
+		titulo = item.a.text
+		url = item.a["href"]
+		iconimage = item.img["src"]
+		addDir("[B]"+titulo.encode('utf-8')+"[/B]",url,11,iconimage,False)
+	xbmc.executebuiltin('Container.SetViewMode(500)')
+	
+def player_master(name,url,iconimage):
+	status = xbmcgui.DialogProgress()
+	status.create('ROGGER STREAM', 'Resolvendo link...','Por favor aguarde...')
+	playlist = xbmc.PlayList(1)
+	playlist.clear()
+	params = url.split(',')
+	status.update(33)
+	try:
+		ip = params[0]
+		playpath = params[1]
+		link = 'rtmp://'+ip+'/live?wmsAuthSign='+get_wms() +' playpath='+playpath+' swfUrl='+url_base3+' live=1 pageUrl='+url_base+' token='+gettoken() +' '
+		listitem = xbmcgui.ListItem(name,thumbnailImage=iconimage)
+		listitem.setInfo("Video", {"Title":name})
+		status.update(66)
+		listitem.setProperty('mimetype', 'video/mp4')
+		playlist.add(link,listitem)	
+		xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+		xbmcPlayer.play(playlist)
+		status.update(100)
+		status.close()
+	except:	
+		xbmcgui.Dialog().ok('ROGGER STREAM', 'Canal temporariamente indisponivel,desculpe o transtorno.')
+
+def	player_series_e_desenhos_24hrs(name,url,iconimage):
+	status = xbmcgui.DialogProgress()
+	status.create('ROGGER STREAM', 'Resolvendo link...','Por favor aguarde...')
+	playlist = xbmc.PlayList(1)
+	playlist.clear()
+	params = url.split(',')
+	status.update(33)
+	try:
+		ip = params[0]
+		playpath = params[1]
+		directory = params[2]
+		html = abrir_url(url_base5+directory)
+		wmsAuthSign = re.compile('tvamigos(.+?)&autostart').findall(html)[0]
+		link = 'rtmp://'+ip+wmsAuthSign+' playpath='+playpath+' swfUrl='+url_base6+' live=1 pageUrl='+url_base5+directory
+		status.update(66)
+		listitem = xbmcgui.ListItem(name,thumbnailImage=iconimage)
+		listitem.setInfo("Video", {"Title":name})
+		listitem.setProperty('mimetype', 'video/mp4')
+		playlist.add(link,listitem)	
+		xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+		xbmcPlayer.play(playlist)
+		status.update(100)
+		status.close()
+	except:	
+		xbmcgui.Dialog().ok('ROGGER STREAM', 'Conteudo temporariamente indisponivel,desculpe o transtorno.')	
+	
+##########################################################################################################################
+
 def abrir_url(url):
 	req = urllib2.Request(url)
 	req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
@@ -92,6 +174,14 @@ def abrir_url(url):
 	link=response.read()
 	response.close()
 	return link
+	
+def gethtml(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link = response.read()
+    soup = BeautifulSoup(link)
+    return soup	
 
 def real_url(url):
 	req = urllib2.Request(url)
@@ -109,20 +199,45 @@ def addLink(name,url,iconimage):
 	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
 	return ok
 
-def addDir(name,url,mode,iconimage,pasta=True,total=1):
-	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-	ok=True
-	liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-	liz.setProperty('fanart_image', fanart)
-	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=pasta,totalItems=total)
-	return ok
-
+#def addDir(name,url,mode,iconimage,pasta=True,total=1):
+	#u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+	#ok=True
+	#liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+	#liz.setProperty('fanart_image', fanart)
+	#ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=pasta,totalItems=total)
+	#return ok
 	
+def addDir(name,url,mode,iconimage,pasta=True,total=1,plot=''):
+	u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)
+	ok=True
+	liz=xbmcgui.ListItem(name, iconImage="iconimage", thumbnailImage=iconimage)
+	liz.setProperty('fanart_image', fanart)
+	liz.setInfo(type="Video", infoLabels={"Title": name, "Plot": plot})
+	contextMenuItems = []
+	contextMenuItems.append(('Movie Information', 'XBMC.Action(Info)'))
+	ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=pasta,totalItems=total)
+	return ok	
+	
+def get_wms():
+	req = urllib2.Request(url_base)
+	req.add_header('referer', url_base2)
+	response = urllib2.urlopen(req)
+	link=response.read()
+	response.close()
+	wms = re.compile(r"AuthSign=(.+?)&auto").findall(link)[0]
+	return wms	
+	
+def gettoken():
+	req = urllib2.Request(base64.b64decode('aHR0cHM6Ly9jb3B5LmNvbS9LYjNSdnY4WnZIQmpOYVZsP2Rvd25sb2FkPTE='))
+	response = urllib2.urlopen(req)
+	token=response.read()
+	response.close()
+	return token	
+
 ############################################################################################################
 #                                               GET PARAMS                                                 #
 ############################################################################################################
 
-              
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -140,14 +255,12 @@ def get_params():
                                 param[splitparams[0]]=splitparams[1]
                                 
         return param
-
-      
+ 
 params=get_params()
 url=None
 name=None
 mode=None
 iconimage=None
-
 
 try:
         url=urllib.unquote_plus(params["url"])
@@ -173,34 +286,50 @@ print "URL: "+str(url)
 print "Name: "+str(name)
 print "Iconimage: "+str(iconimage)
 
-
 ###############################################################################################################
 #                                                   MODOS                                                     #
 ###############################################################################################################
 
-
 if mode==None or url==None or len(url)<1:
     print ""
-    menu()
+    menus()
 	
-elif mode==3:
+elif mode==2:
 	print ""
 	categorias()
 
-elif mode==5:
+elif mode==3:
 	print ""
 	categorias_tv_paga_brasil()	
 	
-elif mode==6: 
+elif mode==4: 
 	print ""
 	listar_canais(url)
 
-elif mode==10: 
+elif mode==5: 
 	listar_videostxt(url)
-    
-elif mode==11:
-	player_youtube(url)	
-
-
 	
+elif mode==6:
+	player_youtube(url)
+
+elif mode==7:
+    print ""
+    listar_categorias()	
+	
+elif mode==8:
+    print ""
+    canais_master(name,url,iconimage)
+	
+elif mode==9:
+    print ""
+    series_e_desenhos_24hrs()	
+	
+elif mode==10:
+    print ""
+    player_master(name,url,iconimage)
+	
+elif mode==11:
+    print ""
+    player_series_e_desenhos_24hrs(name,url,iconimage)	
+
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
